06bad57e6ea1d3734b5f483cdada435ac07cfe33
wanli <hyhgfrgh@163.com>
2026-01-26 12:55:08 +0800

    Add arithmetic series update using second difference

--- 数据结构/前缀和_差分.cpp
+++ 数据结构/前缀和_差分.cpp
@@ -3,61 +3,14 @@
 }
 
 {
-    // 二维前缀和  示例 Smilo and Minecraft https://codeforces.com/contest/2113/problem/C
-    // 求矩形区域目标点最少 示例
-    int n,m,k;cin>>n>>m>>k;
-    vector<string> a(n+1);
-    for(int i = 1;i<=n;i++){
-        cin>>a[i];a[i] = " "+a[i];
-    }
-    vector<vector<int>> pre(n+1,vector<int> (m+1));
-    int sum = 0;
-    for(int i = 1;i<=n;i++){
-        for(int j = 1;j<=m;j++){
-            if(a[i][j] == 'g') pre[i][j]++,sum++;
-            pre[i][j] += pre[i][j-1];
-        }
-        for(int j = 1;j<=m;j++){
-            pre[i][j] += pre[i-1][j];
-        }
-    }
-    int mn = 1e9;
-    for(int i = 1;i<=n;i++){
-        for(int j = 1;j<=m;j++){
-            if(a[i][j] == '.'){
-                int x1 = min(n,i+k),x2 = max(0LL,i-k-1);
-                int y1 = min(m,j+k),y2 = max(0LL,j-k-1);
-                int tt = pre[x1][y1]-pre[x1][y2]-pre[x2][y1]+pre[x2][y2];
-                mn = min(mn,tt);
-            }
-        }
-    }
-}
-
-{
-    // 二维差分  示例
-    int n,m;cin>>n>>m;
-    vector a(n+2,vector<int> (m+2));
-    int q;cin>>q;
-    while(q--){
-        int x1,y1,x2,y2;
-        cin>>x1>>y1>>x2>>y2;
-        a[x1][y1]++;
-        a[x1][y2+1]--;
-        a[x2+1][y1]--;
-        a[x2+1][y2+1]++;
-    }
-    for(int i = 1;i<=n;i++){
-        for(int j = 1;j<=m;j++){
-            a[i][j] += a[i][j-1];
-        }
-        for(int j = 1;j<=m;j++){
-            a[i][j] += a[i-1][j];
-            cout<<a[i][j]<<" ";
-        }
-        cout<<"\n";
-    }
-
+    // 用二次差分在区间[l,r]上加上等差数列：首项s，公差d
+    auto add_arithmetic = [&]( int l, int r, int s, int d) {
+        if(l > r) return;
+        d2[l] += s;
+        if(l+1 <= n+1) d2[l+1] += d - s;
+        if(r+1 <= n+1) d2[r+1] += -(s + (r-l+1)*d);
+        if(r+2 <= n+1) d2[r+2] += d-(s + (r-l+1)*d);
+    };
 }
 
 {
